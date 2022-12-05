9beedf730169714c23269b1be87359a16036d8c4
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-12-05 14:07:03 -0800

    Add the tenth task

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    else:
-        max_num = my_list[0]
-        for i in range(len(my_list)):
-            if my_list[i] > max_num:
-                max_num = my_list[i]
-    return max_num
+    largest = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > largest:
+            largest = my_list[i]
+    return largest
